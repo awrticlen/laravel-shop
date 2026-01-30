551efaf6650653a6548d6cf9340bb807d61f948b
awrticlen <12121212article@sina.com>
2026-01-30 08:49:09 +0000

    邮箱验证功能

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -4,8 +4,8 @@
     <a class="navbar-brand " href="{{ url('/') }}">
       Laravel Shop
     </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
     </button>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -21,8 +21,8 @@
         <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">注册</a></li>
         @else
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          <img src="https://www.gravatar.com/avatar/{{ md5(Auth::user()->email) }}?d=identicon&s=60" class="img-responsive img-circle" width="30px" height="30px" alt="{{ Auth::user()->name }}">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <img src="https://www.gravatar.com/avatar/{{ md5(Auth::user()->email) }}?d=identicon&s=60" class="img-responsive img-circle" width="30px" height="30px" alt="{{ Auth::user()->name }}">
                       {{ Auth::user()->name }}
           </a>
           <div class="dropdown-menu" aria-labelledby="navbarDropdown">
