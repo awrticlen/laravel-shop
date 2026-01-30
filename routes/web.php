8161bedc0e053008ef2a998976f98dd9d0ca0213
awrticlen <12121212article@sina.com>
2026-01-30 09:48:26 +0000

    收货地址列表

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PagesController;
+use App\Http\Controllers\UserAddressesController; // 新增这一行
 
 Route::get('/', [PagesController::class, 'root'])->name('root');
 Auth::routes(['verify' => true]);
+
+// auth 中间件代表需要登录，verified中间件代表需要经过邮箱验证
+Route::group(['middleware' => ['auth', 'verified']], function () {
+    Route::get('user_addresses', [UserAddressesController::class, 'index'])
+        ->name('user_addresses.index');
+});
