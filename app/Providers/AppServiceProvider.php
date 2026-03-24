0f7f40f2361282f1a176ba28b36b421c839b8336
awrticlen <12121212article@sina.com>
2026-03-24 04:36:32 +0000

    完善支付回调

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Yansongda\Pay\Pay;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,9 +24,11 @@ class AppServiceProvider extends ServiceProvider
         // 往服务容器中注入一个名为 alipay 的单例对象
         $this->app->singleton('alipay', function() use ($config) {
             //调用Yansongda\Pay来创建一个支付宝支付对象
-            $config               = config('pay.alipay');
-            $config['notify_url'] = route('payment.alipay.notify');
-            $config['return_url'] = route('payment.alipay.return');
+            $config = config('pay');
+            $config['alipay']['default']['notify_url'] = ngrok_url('payment.alipay.notify');
+            $config['alipay']['default']['return_url'] = ngrok_url('payment.alipay.return');
+
+            return Pay::alipay($config);
         });
 
         $this->app->singleton('wechat_pay', function() use ($config) {
@@ -39,6 +42,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // 支付宝/微信等服务器回调不会携带 CSRF Token，需放入白名单
+        VerifyCsrfToken::except([
+            'payment/alipay/notify',
+        ]);
+
         \Illuminate\Pagination\Paginator::useBootstrap();
     }
 }
