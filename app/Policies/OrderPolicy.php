48eee8a2c568d81256a31f2b975a4f42ebfde2e6
awrticlenjia <765406912@qq.com>
2026-05-17 15:53:18 +0000

    将商品模型转为 Elasticsearch 文档数组

--- app/Policies/OrderPolicy.php
+++ app/Policies/OrderPolicy.php
@@ -11,12 +11,7 @@ use Illuminate\Auth\Access\HandlesAuthorization;
 class OrderPolicy
 {
     use HandlesAuthorization;
-
-    public function own(AuthUser $authUser, Order $order): bool
-    {
-        return (int) $order->user_id === (int) $authUser->id;
-    }
-
+    
     public function viewAny(AuthUser $authUser): bool
     {
         return $authUser->can('ViewAny:OrderResource');
