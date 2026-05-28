9d3a6ecd501588750fe647459ff71fac648a2fdc
awrticlenjia <765406912@qq.com>
2026-05-28 12:48:17 +0000

    DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,9 +16,9 @@ class DatabaseSeeder extends Seeder
         $this->call(ShieldSeeder::class);
         $this->call(UsersSeeder::class);
         $this->call(UserAddressesSeeder::class);
-        // 放在 ProductsSeeder 之前
+        // 放在商品 Seeder 之前（DDRProductsSeeder 依赖「内存」类目）
         $this->call(CategoriesSeeder::class);
-        $this->call(ProductsSeeder::class);
+        $this->call(DDRProductsSeeder::class);
         $this->call(ProductsSeeder::class);
         $this->call(CouponCodesSeeder::class);
         $this->call(OrdersSeeder::class);
